06b16204b90bffda7e046feb53c66bc6bac7dde8
Cristian Tironi <ctironi@estudiante.ceibal.edu.uy>
2022-06-27 16:06:36 -0300

    Primer Ejercicio listo

--- src/main/java/com/sofka/proyect/Ejercicio1.java
+++ src/main/java/com/sofka/proyect/Ejercicio1.java
@@ -2,5 +2,16 @@ package com.sofka.proyect;
 
 public class Ejercicio1 {
 
-    
+    public static void main(String[] args) {
+        int nroA = 5;
+        int nroB = 6;
+
+        if(nroA == nroB){
+            System.out.println("El valor de ambos numeros es igual: A=" + nroA + " y B=" + nroB );
+        }else if (nroA > nroB){
+            System.out.println("El valor de A es mayor que B: " + nroA);
+        }else{
+            System.out.println("El valor de B es mayor que A: " + nroB);
+        }
+    }
 }
